fff9c9324cf742f510067876527304cb83955e25
Nobohitoshi <dengchuanrenzhi@gmail.com>
2019-05-21 16:19:19 +0900

    js　スクロール

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+$(function () {
+
+    $(`.arrow`).click(function () {
+
+        $(`html`).animate({scrollTop: 0}, 1000);
+    })
+
+    $(window).scroll(function() {
+
+        let scrollTopValue = $(document).scrollTop();
+        
+
+        if (scrollTopValue > 500) {
+
+            $(`.arrow`).fadeIn();
+        } else {
+
+            $(`.arrow`).fadeOut();
+        }
+    })
+
+})
